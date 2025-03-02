49bac0dfc343326c106c21292846cab8c4fc61c5
CedricMartina <146848732+CedricMartina@users.noreply.github.com>
2025-03-02 10:10:15 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const progressBar = document.getElementById('progress-bar');
+
+    // Animate the progress bar
+    setTimeout(() => {
+        progressBar.style.width = '100%';
+    }, 500);
+});
